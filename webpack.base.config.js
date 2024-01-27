58ef352db91c188e98cd75f9a45d8077ae9a1f1e
linyuchen <lin.yu.chen@foxmail.com>
2024-01-27 08:33:03 +0800

    删除webpack多余注释

--- webpack.base.config.js
+++ webpack.base.config.js
@@ -1,22 +1,16 @@
-// import path from "path";
 const path = require('path');
 const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
     // target: 'node',
     entry: {
-        // main: './src/main.ts',
-        // preload: './src/preload.ts'
     }, // 入口文件路径
     target: "node",
     output: { // 输出文件配置
         path: path.resolve(__dirname, 'dist'), // 输出目录路径
         filename: '[name].js', // 输出文件名
-        // libraryTarget: "commonjs2",
-        // chunkFormat: "commonjs",
     },
     externals: [
-        // "express",
         "electron", "fs"],
     experiments: {
         // outputModule: true
